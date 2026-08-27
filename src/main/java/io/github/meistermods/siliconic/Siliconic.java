1cd93232b5c352638f8a98faa1b7908baffd6b36
elecbug <deveb1479@gmail.com>
2026-08-27 11:17:55 +0900

    feat: initialize Minecraft mod project structure

---
+++ src/main/java/io/github/meistermods/siliconic/Siliconic.java
@@ -0,0 +1,12 @@
+package io.github.meistermods.siliconic;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Siliconic.MOD_ID)
+public final class Siliconic {
+  public static final String MOD_ID = "siliconic";
+
+  public Siliconic(FMLJavaModLoadingContext context) {
+  }
+}
